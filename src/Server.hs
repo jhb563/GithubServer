9ae000501010627b452c852fdb6f54988141fb58
James Bowen <jhbowen047@gmail.com>
2018-05-08 06:40:35 -0700

    Added second endpoint

--- src/Server.hs
+++ src/Server.hs
@@ -7,22 +7,32 @@ module Server
   ( runServer
   ) where
 
+import Control.Monad.IO.Class (liftIO)
 import Data.Proxy (Proxy(..))
 import Network.Wai.Handler.Warp (run)
 import System.Environment (getEnv)
 import Servant.API
 import Servant.Server
 
-type MyAPI = "api" :> "ping" :> Get '[JSON] String
+type MyAPI = 
+  "api" :> "ping" :> Get '[JSON] String
+  :<|> "api" :> "hook" :> ReqBody '[JSON] () :> Post '[JSON] ()
 
 pingHandler :: Handler String
-pingHandler = return "Hello World!!!!??"
+pingHandler = do
+  liftIO $ print "Received a ping!"
+  return "Hello World!!!!??"
+
+hookHandler :: () -> Handler ()
+hookHandler _ = do
+  liftIO $ print "Received a hook!"
+  return ()
 
 myAPI :: Proxy MyAPI
 myAPI = Proxy :: Proxy MyAPI
 
 myServer :: Server MyAPI
-myServer = pingHandler
+myServer = pingHandler :<|> hookHandler
 
 runServer :: IO ()
 runServer = do
